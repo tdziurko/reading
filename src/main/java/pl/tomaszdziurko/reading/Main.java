73d92b7b4fdf74c0afc4b24fdac8759c28670e64
Tomasz Dziurko <tdziurko@gmail.com>
2013-11-03 10:34:27 +0100

    Data script generating now starts one day earlier so two lines in graph have common start

--- src/main/java/pl/tomaszdziurko/reading/Main.java
+++ src/main/java/pl/tomaszdziurko/reading/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     private DateTime startDate;
     private int pages;
+    private int pagesPerDay;
 
     public static void main(String[] args) {
 
@@ -16,13 +17,14 @@ public class Main {
 //        Main main = new Main(args[0], Integer.parseInt(args[1]));
 
 
-        Main main = new Main("30/10/2013", 335);
+        Main main = new Main("30/10/2013", 335, 10);
 
         main.run();
     }
 
-    public Main(String dateString, int pages) {
-        startDate = DateTimeFormat.forPattern("dd/MM/yyyy").parseDateTime(dateString);
+    public Main(String dateString, int pages, int pagesPerDay) {
+        this.pagesPerDay = pagesPerDay;
+        startDate = DateTimeFormat.forPattern("dd/MM/yyyy").parseDateTime(dateString).minusDays(1);
         this.pages = pages;
     }
 
@@ -30,12 +32,14 @@ public class Main {
 
         DateTime currentDate = new DateTime(startDate);
         int currentPage = pages;
+        String currentValue = pages + "";
         do {
             String dateString = currentDate.toString("dd/MM");
 
-            System.out.println(String.format("['%5s',   %4s,   %4s], ", dateString, currentPage, "null"));
-            currentPage = currentPage - 10;
+            System.out.println(String.format("['%5s',   %4s,   %4s], ", dateString, currentPage, currentValue));
+            currentPage = currentPage - pagesPerDay;
             currentDate = currentDate.plusDays(1);
+            currentValue = "null";
 
             if (currentPage <= 0) {
                 System.out.println(String.format("['%5s',      0,   %4s] ", dateString, "null"));
